e9599bbd305fe2e6156a70f141f335b16a1085db
Ali FateFul <ali.fateful21@gmail.com>
2024-04-09 11:16:26 +0330

    Products Page extracted

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,29 @@
+import { FaListUl } from "react-icons/fa";
+
+import { createQueryObject } from "../helper/helper";
+
+function SideBar({ setQuery }) {
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    setQuery((query) => createQueryObject(query, { category }));
+  };
+  return (
+    <div>
+      <div>
+        <FaListUl />
+        <p>Categories</p>
+      </div>
+      <ul onClick={categoryHandler}>
+        <li>All</li>
+        <li>Electronics</li>
+        <li>Jewelery</li>
+        <li>Men's Clothing</li>
+        <li>Women's Clothing</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SideBar;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 //Icons
-import { ImSearch } from "react-icons/im";
-import { FaListUl } from "react-icons/fa";
 //Context
 import { useProducts } from "../contexts/produtsContext";
 //Style
@@ -10,12 +8,13 @@ import styles from "./ProductsPage.module.css";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import {
-  createQueryObject,
   filterProducts,
   getInitialQuery,
   searchProducts,
 } from "../helper/helper";
 import { useSearchParams } from "react-router-dom";
+import SearchBox from "../components/SearchBox";
+import SideBar from "../components/SideBar";
 
 function ProductsPage() {
   const products = useProducts();
@@ -39,29 +38,9 @@ function ProductsPage() {
     setDisplayed(finalProducts);
   }, [query]);
 
-  const searchHadler = () => {
-    setQuery((query) => createQueryObject(query, { search }));
-  };
-  const categoryHandler = (e) => {
-    const { tagName } = e.target;
-    const category = e.target.innerText.toLowerCase();
-    if (tagName !== "LI") return;
-    setQuery((query) => createQueryObject(query, { category }));
-  };
-
   return (
     <>
-      <div>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
-        />
-        <button onClick={searchHadler}>
-          <ImSearch />
-        </button>
-      </div>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
       <div className={styles.container}>
         <div className={styles.products}>
           {!displayed.length && <Loader />}
@@ -69,19 +48,7 @@ function ProductsPage() {
             <Card key={p.id} data={p} />
           ))}
         </div>
-        <div>
-          <div>
-            <FaListUl />
-            <p>Categories</p>
-            <ul onClick={categoryHandler}>
-              <li>All</li>
-              <li>Electronics</li>
-              <li>Jewelery</li>
-              <li>Men's Clothing</li>
-              <li>Women's Clothing</li>
-            </ul>
-          </div>
-        </div>
+        <SideBar setQuery={setQuery} />
       </div>
     </>
   );
