0cdd2ee415793929a69278a4f72740b034048e1d
Ali FateFul <ali.fateful21@gmail.com>
2024-04-11 23:42:17 +0330

    CheckOutPage Developed and the Project was completed

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSidebar";
+import { useCart } from "../contexts/CartContext";
+
+import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
+  const [state, dispatch] = useCart();
+  const clickHandler = (type, payload) => {
+    dispatch({ type, payload });
+  };
+
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
+    );
+  }
+
   return (
-    <div>CheckoutPage</div>
-  )
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default CheckoutPage
+export default CheckoutPage;
